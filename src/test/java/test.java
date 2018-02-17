33a3fbc524e29da93b8d0e0c97e91a17f2600df4
Phage, Muzi PE <muzi.phage@standardbank.co.za>
2018-02-17 10:54:13 +0200

    Added assertions and test annotations

--- src/test/java/test.java
+++ src/test/java/test.java
@@ -1,11 +1,17 @@
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 import java.io.File;
 
+import static org.junit.Assert.assertEquals;
+
 public class test {
 
+    @Test
     public static void main(String[]args) throws InterruptedException {
 
         /*File file = new File("/home/muzi/temp/chromedriver");
@@ -19,6 +25,10 @@ public class test {
         Thread.sleep(10000);
         //driver.navigate().to("http://10.182.218.194/EAppIntranet/flow/capture/termsnca.aspx");
         driver.navigate().to("http://www.google.com");
+
+        String actualTitle = driver.getTitle();
+        String expectedTitle = "Google";
+        assertEquals(actualTitle, expectedTitle);
     }
 
 }
